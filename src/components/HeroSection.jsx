b8ee403869717bbc07f3d13699afeb9bedd507f6
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-21 17:08:26 +0100

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 export default function HeroSection() {
   return (
     <section className='md:flex w-screen min-h-full '>
-      <div className='w-full md:w-3/6 m-auto  p-6 md:p-10 lg:p-16 pb-8   '>
+      <div className='w-full md:w-3/6 m-auto  p-6 md:p-10 lg:p-16 pb-8 md:mb-6  '>
         <h1 className='text-7xl  tracking-wider mt-10 '>One Platform <br /> to
             <span className='text-zoom-blue '> connect</span>
          </h1>
         <p className='text-lg mt-14 font-semibold tracking-wider'>
             Bring teams together, reimagine workspaces, engage new audiences, and delight your customers — all on the Zoom platform you know and love.
         </p>
-        <div className=' mt-16 text-center '>
+        <div className=' mt-10 text-center '>
             <button className='bg-blue-500 hover:bg-zoom-blue px-8 rounded-3xl py-3 text-white text-lg' type='button'>Plans & Pricing</button>
         </div>
       </div>
